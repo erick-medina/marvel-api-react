42ae3538cb3660a6f53b40aa025f8d0691f843b4
Erick Medina <erickmedinabe@gmail.com>
2020-05-06 11:50:15 +0200

    I was having issues with installing sass, but I could fixed it with install --save-dev --unsafe-perm node-sass. Dunno if it was the right thing to do but it worked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="display-1">Hello world!</h1>
+      <h1 className="display-1 ">Hello world!</h1>
     </div>
   );
 }
